9f828be8d5224b317ea7725e689f833a9972f2c9
deshinta <deshintaaulia065@gmail.com>
2024-05-24 11:54:05 +0700

    feat:add validation for myAccount

--- controller/controllerSend.js
+++ controller/controllerSend.js
@@ -4,56 +4,67 @@ const { PaymentAccount,User,PaymentHistory } = require('../models/')
 class ControllerSend {
     static async postSend(req,res){
         try {
-            const { amount,toAddress } = req.body
-            let sender = await PaymentAccount.findByPk(req.user.id)
-            let receiver = await PaymentAccount.findByPk(toAddress)
-            if(!sender || !receiver){
-                res.status(500).json({message:'Internal Server Error'})
+            const { myAccount,amount,toAddress } = req.body
+            let sender = await PaymentAccount.findByPk(myAccount)
+            if(sender.UserId!==req.user.id){
+                res.status(403).json({message:"Forbidden"})
             } else {
-                let amountSender = sender.amount - Number(amount)
-                let amountReceiver = receiver.amount + Number(amount)
-                // console.log({amount,amountSender,amountReceiver});
-                amountSender = await PaymentAccount.update(
-                    {amount:amountSender},
-                    {where:{id:req.user.id}}
-                )
-                amountReceiver = await PaymentAccount.update(
-                    {amount:amountReceiver},
-                    {where:{id:toAddress}}
-                )
-                if(!amountSender || !amountReceiver){
-                    await PaymentAccount.update(
-                        {amount:sender.amount},
+                let receiver = await PaymentAccount.findByPk(toAddress)
+                if(!sender || !receiver){
+                    res.status(500).json({message:'Internal Server Error'})
+                } else {
+                    let amountSender = sender.amount - Number(amount)
+                    let amountReceiver = receiver.amount + Number(amount)
+                    // console.log({amount,amountSender,amountReceiver});
+                    amountSender = await PaymentAccount.update(
+                        {amount:amountSender},
                         {where:{id:req.user.id}}
                     )
-                    await PaymentAccount.update(
-                        {amount:receiver.amount},
+                    amountReceiver = await PaymentAccount.update(
+                        {amount:amountReceiver},
                         {where:{id:toAddress}}
                     )
-                    res.status(400).json({message:'Bad Request'})
-                } else {
-                    await PaymentHistory.create({name:'send user',amount, SenderId:req.user.id, toAddress, status: 'send'})
-                    let transaction = { amount:req.body.amount, currency:'USD'}
-                    processTransaction(transaction)
-                    .then((processedTransaction) => {
-                        // console.log('transaction processing completed for:', processedTransaction);
-                        res.status(201).json({message:'transaction processing completed for:', processedTransaction})
-                    })
-                    .catch((error) => {
-                        console.error('transaction processing failed:', error);
-                        res.status(500).json({message:'Internal Server Error'})
-                    });
-                }
-
-            }
+                    if(!amountSender || !amountReceiver){
+                        await PaymentAccount.update(
+                            {amount:sender.amount},
+                            {where:{id:req.user.id}}
+                        )
+                        await PaymentAccount.update(
+                            {amount:receiver.amount},
+                            {where:{id:toAddress}}
+                        )
+                        res.status(400).json({message:'Bad Request'})
+                    } else {
+                        await PaymentHistory.create({name:'send user',amount, SenderId:req.user.id, toAddress, status: 'send'})
+                        let transaction = { amount:req.body.amount, currency:'USD'}
+                        processTransaction(transaction)
+                        .then((processedTransaction) => {
+                            // console.log('transaction processing completed for:', processedTransaction);
+                            res.status(201).json({message:'transaction processing completed for:', processedTransaction})
+                        })
+                        .catch((error) => {
+                            console.error('transaction processing failed:', error);
+                            res.status(500).json({message:'Internal Server Error'})
+                        });
+                    }
     
+                }
+        
+            }
         } catch (error) {
             res.status(500).json({message:'Internal Server Error'})
         }
     }
     static async getAccountIncludeUser(req,res){
         try {
-            let data = await PaymentAccount.findAll({include:PaymentHistory})
+            let data = await PaymentAccount.findAll(
+                {
+                    include:PaymentHistory,
+                    where:{
+                        UserId:req.user.id
+                    }
+                }
+            )
             res.status(200).json(data)
         } catch (error) {
             // console.log(error,'<< error get payment');
